b51894faf3a68d5de15461bee023538572fd47a9
kmtc44 <kairmor@yahoo.com>
2020-07-05 20:23:29 +0000

    feat
    react ssr boiterplate app

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class app extends Component {
+  render() {
+    return (
+      <div>
+        react ssr boilerplate js
+      </div>
+    )
+  }
+}
