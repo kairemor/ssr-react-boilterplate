9048b89d9dc05b4cb4a01610e862be1e7b802bf3
kmtc44 <kairmor@yahoo.com>
2020-07-08 12:08:28 +0000

    ssr i18n backend

--- src/app.js
+++ src/app.js
@@ -1,13 +1,42 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
+import i18next from 'i18next';
+import middleware from 'i18next-express-middleware';
+import nodeFsBackend from 'i18next-node-fs-backend';
 import template from './shared/template';
 import ssr from './server/server';
 
 const app = express();
 
-// Serving static files
-app.use('', express.static(path.resolve(__dirname, '../assets')));
 app.use('/build', express.static(path.resolve(__dirname, '../build')));
+
+// Init i18n
+i18next
+  .use(middleware.LanguageDetector)
+  .use(nodeFsBackend)
+  .init({
+    debug: false,
+    lng: 'en',
+    fallbackLng: 'en',
+    lowerCaseLng: true,
+    preload: ['en'],
+    backend: {
+      loadPath: path.join(`${__dirname}../assets/locales/{{lng}}/{{ns}}.json`),
+    },
+    useCookie: false,
+  });
+
+// setup i18n for app
+app.use(
+  middleware.handle(i18next, {
+    removeLngFromUrl: false,
+  })
+);
+
+// cors configuration
+app.use(cors());
+app.options('*', cors());
 
 // hide powered by express
 app.disable('x-powered-by');
@@ -19,6 +48,9 @@ app.listen(process.env.PORT || 3000, () => {
 const initialState = {
   isFetching: false,
 };
+
+// Serving static files
+app.use('', express.static(path.resolve(__dirname, '../assets')));
 
 // server rendered home page
 app.get('/', (req, res) => {
